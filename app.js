6e678cefe07e6f50c9eb3e8366f7b978fc4011bc
Sarah Quayyum <95451775+qymmore@users.noreply.github.com>
2022-05-11 13:08:29 -0400

    update display screen function

--- app.js
+++ app.js
@@ -9,10 +9,13 @@ const displayScreen = document.querySelector('.display-screen');
 const currentOperand = document.querySelector('.current-operand');
 const previousOperand = document.querySelector('.previous-operand');
 
+currentOperand.textContent = '';
+previousOperand.textContent = '';
+
 const calculator = {
-    displayValue: '0',
+    displayValue: '0', //default display screen value should be this
     firstOperand: null,
-    secondOperand: false,
+    secondOperand: null,
     chosenOperator: null,
 }
 
@@ -22,30 +25,61 @@ function defaultDisplay() {
 }
 defaultDisplay();
 
-//display clicked numbers and operators on screen
-function displayNumbers () {
+
+//display clicked numbers and operators on screen and also save their values 
+function displayAll () {
     numberButtons.forEach((number) => {
         number.addEventListener('click', function() {
             if(displayScreen.textContent === '0') {
                 displayScreen.textContent = number.value; //if display shows 0 change the display value to the number pressed
+                calculator.firstOperand = displayScreen.textContent; //store the first number in firstOperand
+                console.log(calculator.firstOperand);
+            } else if(calculator.firstOperand != null && calculator.secondOperand === null && calculator.chosenOperator != null) {
+                calculator.displayValue = number.value;
+                displayScreen.textContent += calculator.displayValue;
+                calculator.secondOperand = displayScreen.textContent; //store secondOperand if operator and first operand are not null
+                console.log(calculator.secondOperand);
             } else {
                 calculator.displayValue = number.value;
                 displayScreen.textContent += calculator.displayValue;
+                calculator.firstOperand = displayScreen.textContent; //store all consecutive digits into firstOperand
+                console.log(calculator.firstOperand);
+            }
+        })
+    });
+    operatorButtons.forEach((operator) => {
+        operator.addEventListener('click', function() {
+            if(calculator.firstOperand != null && calculator.chosenOperator === null) { //if calculator display is not zero and firstOperand is not null
+                calculator.displayValue = operator.value; //display value will be the operator value pressed
+                displayScreen.textContent += calculator.displayValue; //update display screen
+                calculator.chosenOperator = displayScreen.textContent; //save operator to chosenOperator
+                console.log(calculator.chosenOperator);
+            } else {
+                alert("ERROR CANNOT HAVE MORE THAN TWO OPERANDS AND AN OPERATOR FOR NOW");
             }
         })
     });
 }
-displayNumbers();
 
-function displayOperators () {
-    operatorButtons.forEach((operator) => {
-        operator.addEventListener('click', function() {
-            calculator.displayValue = operator.value;
-            displayScreen.textContent += calculator.displayValue;
-        })
+displayAll();
+
+//run the evaluate function when clicking on equal button
+/*equalsButton.addEventListener('click', function() {
+    evaluate();
+});*/
+
+//clear the display screen back to 0
+function clearDisplay() {
+    clearButton.addEventListener('click', () => {
+        if(displayScreen.textContent != "0") {
+            displayScreen.textContent = '0';
+        } else {
+            return;
+        }
     });
-}
-displayOperators();
+};
+clearDisplay();
+
 
 function add(a, b) {
     return a + b;
@@ -86,7 +120,11 @@ function operate(operator, a , b) {
             return multiply(a, b);
         case '÷':
             if(b === 0) return null
-            else return divide(a, b)
+            else return divide(a, b);
+        case '%':
+            return percent(a);
+        case '√':
+            return squareRoot(a);
         default:
             return null;
     }
